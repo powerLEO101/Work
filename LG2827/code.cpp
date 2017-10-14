b65d3c93c10354f368e822c16d212a6fbe4188d5
powerLEO101 <3256870400@qq.com>
2017-10-14 20:52:12 +0800

    写出了LG2827蚯蚓的代码，但没有对

--- LG2827/code.cpp
+++ LG2827/code.cpp
@@ -3,6 +3,7 @@
 #include<fstream>
 #include<cstdio>
 #include<algorithm>
+#include<queue>
 #define INF 0x3f3f3f3f
 #define gi get_int()
 int get_int()
@@ -15,10 +16,10 @@ int get_int()
 }
 
 
-int Temp[10001];
+int Temp[10001],Len = 0;
 std::queue<int> q1,q2,q3;
 
-int Front(std::queue<int>& a){return a.empty()?-INF:a.front();}
+int Front(std::queue<int>& a){return a.empty()?-INF:a.front()+Len;}
 int Get()
 {
 	int Max = std::max(Front(q1),std::max(Front(q2),Front(q3)));
@@ -28,18 +29,29 @@ int Get()
 	return Max;
 }
 
-int cmp(int& aa,int& bb){return aa>bb;}
+int cmp(const int& aa,const int& bb){return aa>bb;}
 
 int main()
 {
 	int n = gi,m = gi,q = gi,u = gi,v = gi,t = gi;
 	for(int i = 0;i<n;i++)Temp[i] = gi;
-	std::sort(a,a+n,cmp);
-	for(int i = 0;i<n;i++)q1.push(a[i]);
+	std::sort(Temp,Temp+n,cmp);
+	for(int i = 0;i<n;i++)q1.push(Temp[i]);
 	for(int i = 0;i<m;i++)
 	{
 		int k = Get();
-		
+		int Temp1 = 1ll*k*u/v;
+		int Temp2 = k-Temp1;
+		q2.push(Temp1-Len-q);
+		q3.push(Temp2-Len-q);
+		if((i+1)%t==0)std::cout<<k<<"$";
+		Len+=q;
+	}
+	std::cout<<std::endl;
+	for(int i = 0;i<n+m;i++)
+	{
+		int k = Get();
+		if((i+1)%t==0)std::cout<<k<<"%";
 	}
 	return 0;
 }
