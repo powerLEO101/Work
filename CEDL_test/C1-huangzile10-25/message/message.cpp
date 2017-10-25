cc6f1ed885408fd86b7f877833c4ae02ccf933b5
powerLEO101 <3256870400@qq.com>
2017-10-25 14:33:53 +0800

    更行了考试代码

---
+++ CEDL_test/C1-huangzile10-25/message/message.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<iomanip>
+#include<fstream>
+#include<cstdio>
+#include<algorithm>
+
+int main()
+{
+	freopen("message.in","r",stdin);
+	freopen("message.out","w",stdout);
+	std::cout<<"这题不是人做的！！！";
+	return 0;
+}
---
+++ CEDL_test/C1-huangzile10-25/message/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+while true
+do
+	g++ message.cpp -o message
+	./message
+done
