78a407cc9c4803d343f1040b2382b23250314fe3
powerLEO101 <3256870400@qq.com>
2017-11-01 09:28:43 +0800

    Create qiou.cpp

--- CEDL_test/C1-huangzile11-01/qiou/qiou.cpp
+++ CEDL_test/C1-huangzile11-01/qiou/qiou.cpp
@@ -17,6 +17,7 @@ public:
 	int m,s;
 }Nodes[1000001];
 int Num[1000001];
+long long Sum[1000001];
 bool operator<(const Node& a,const Node& b){
 	return a.m<b.m;
 }
@@ -31,15 +32,14 @@ int main(){
 	}
 	std::sort(Nodes,Nodes+n);
 	for(int i = 0;i<n;i++)Num[i] = Nodes[i].m;
+	Sum[0] = Nodes[0].s;
+	for(int i = 1;i<n;i++)Sum[i] = Sum[i-1]+Nodes[i].s;
 	long long Ans = -1;
 	for(int i = 0;i<n;i++)
 	{
 		int len = (std::lower_bound(Num,Num+n,Num[i]+m)-Num);
-		long long Temp = 0;
-		for(int j = i;j<len;j++)Temp+=Nodes[j].s;
-		if(Ans<Temp)
-			std::cout<<i<<" "<<Temp<<" "<<len<<std::endl;
-		Ans = std::max(Ans,Temp);
+		if(i==0)Ans = std::max(Ans,Sum[len-1]);
+		else Ans = std::max(Ans,Sum[len-1]-Sum[i-1]);
 	}
 	std::cout<<Ans;
 	return 0;
