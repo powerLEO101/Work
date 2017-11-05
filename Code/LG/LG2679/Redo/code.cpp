8ad27cc29853ead42bc990a31e298330bb4cc2c2
powerLEO101 <3256870400@qq.com>
2017-11-05 15:59:59 +0800

    Changes to be committed: 	deleted:    Ubuntu_install_zsh 	modified:   "miscellaneous_summary(\346\235\202)"

---
+++ Code/LG/LG2679/Redo/code.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<iomanip>
+#include<fstream>
+#include<cstdio>
+#include<algorithm>
+#define gi get_int()
+int get_int()
+{
+	int x = 0,y = 1;char ch = getchar();
+	while((ch<'0'||ch>'9')&&ch!='0')ch = getchar();
+	if(ch=='-')y = -1,ch = getchar();
+	while(ch<='9'&&ch>='0')x = x*10+ch-'0',ch = getchar();
+	return x*y;
+}
+
+int main()
+{
+	freopen("code.in","r",stdin);
+	freopen("code.out","w",stdout);
+	return 0;
+}
+
---
+++ Code/LG/LG2679/Redo/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+while true
+do
+	g++ code.cpp -o code
+	./code
+	sleep 0.5s
+done
