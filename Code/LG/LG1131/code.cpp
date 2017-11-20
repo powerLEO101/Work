f4e50e1ed8088583a2340b14c3deb9933fcb1f07
powerLEO101 <3256870400@qq.com>
2017-11-20 20:45:21 +0800

    new file:   Code/LG/LG1131/code.cpp 	new file:   Code/LG/LG1131/run.sh code.cpp --> Writing 不需要加: Changes to be committed:

---
+++ Code/LG/LG1131/code.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<iomanip>
+#include<fstream>
+#include<cstdio>
+#include<algorithm>
+#include<vector>
+#define gi get_int()
+#define INF 0x3f3f3f3f
+int get_int()
+{
+	int x = 0,y = 1;char ch = getchar();
+	while((ch<'0'||ch>'9')&&ch!='0')ch = getchar();
+	if(ch=='-')y = -1,ch = getchar();
+	while(ch<='9'&&ch>='0')x = x*10+ch-'0',ch = getchar();
+	return x*y;
+}
+
+class Node
+{
+public:
+	int v,w;
+}
+std::vector<Node> Map[500001];
+
+int Dp[500001];
+bool Vis[500001];
+void Get_ans(int Now)
+{
+	int Count = 0,Sum = 0,Max = -INF;
+	for(int i = 0;i<Map[Now].size();i++)
+	{
+		int v = Map[Now][i].v,w = Map[Now][i].w;
+		if(Vis[v]==true)continue;
+		Count++;
+		Sum-=w;
+		Max = std::max(Max,w);
+		Vis[v] = true;
+		Get_ans(v);
+		Dp[Now]+=Dp[v];
+	}
+	if(Count==0)Dp[Now] = 0;
+	else Sum+=Count*Max;
+}
+
+int main()
+{
+	freopen("code.in","r",stdin);
+	freopen("code.out","w",stdout);
+	int n = gi,m = gi;
+	for(int i = 1;i<n;i++)
+	{
+		int u = gi,v = gi,w = gi;
+		Map[u].push_back((Node){v,w});
+		Map[v].push_back((Node){u,w});
+	}
+	
+	return 0;
+}
