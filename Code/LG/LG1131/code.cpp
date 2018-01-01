4726e08025801ef0547d183b011f5cc29a26eb35
powerLEO101 <3256870400@qq.com>
2018-01-01 17:12:50 +0800

    modified:   Code/LG/LG1131/code.cpp Code.cpp --> AC

--- Code/LG/LG1131/code.cpp
+++ Code/LG/LG1131/code.cpp
@@ -3,13 +3,12 @@
 #include<fstream>
 #include<cstdio>
 #include<algorithm>
-#include<vector>
+#include<vector> 
 #define gi get_int()
-#define INF 0x3f3f3f3f
 int get_int()
 {
 	int x = 0,y = 1;char ch = getchar();
-	while((ch<'0'||ch>'9')&&ch!='0')ch = getchar();
+	while((ch<'0'||ch>'9')&&ch!='-')ch = getchar();
 	if(ch=='-')y = -1,ch = getchar();
 	while(ch<='9'&&ch>='0')x = x*10+ch-'0',ch = getchar();
 	return x*y;
@@ -19,51 +18,44 @@ class Node
 {
 public:
 	int v,w;
-};
+}; 
 std::vector<Node> Map[500001];
+int f[500001];
+long long Ans=0;
 
-int Dp[500001];
-bool Vis[500001];
-void Get_ans(int Now)
+void Get_ans(int Now,int Pre)
 {
-	int Count = 0,Sum = 0,Max = -INF;
+	//f[Now] = 0;
 	for(int i = 0;i<Map[Now].size();i++)
 	{
 		int v = Map[Now][i].v,w = Map[Now][i].w;
-		if(Vis[v]==true)continue;
-		Count++;
-		Sum-=w;
-		Max = std::max(Max,w);
-		Vis[v] = true;
-		Get_ans(v);
-		Dp[Now]+=Dp[v];
-	}
-	if(Count==0)
-	{
-		std::cout<<Now+1<<std::endl;
-		Dp[Now] = 0;
-	}
-	else
-	{
-		Sum+=Count*Max;
-		std::cout<<Now+1<<" "<<Sum<<std::endl;
-		Dp[Now]+=Sum;
+		if(v==Pre)continue;
+		Get_ans(v,Now);
+		f[Now] = std::max(f[Now],f[v]+w);
 	}
 }
-
+void dfs(int Now,int Pre)
+{
+	for(int i = 0;i<Map[Now].size();i++)
+	{
+		int v = Map[Now][i].v,w = Map[Now][i].w;
+		if(v==Pre)continue;
+		Ans+=f[Now]-f[v]-w;
+		dfs(v,Now);
+	}
+}
 int main()
 {
 	freopen("code.in","r",stdin);
 	freopen("code.out","w",stdout);
-	int n = gi,m = gi-1;
-	Vis[m] = true;
-	for(int i = 1;i<n;i++)
+	int n = gi,s = gi-1;
+	for(int i = 0;i<n-1;i++)
 	{
 		int u = gi-1,v = gi-1,w = gi;
 		Map[u].push_back((Node){v,w});
 		Map[v].push_back((Node){u,w});
 	}
-	Get_ans(m);
-	for(int i = 0;i<n;i++)std::cout<<Dp[i]<<" ";
+	Get_ans(s,-1);dfs(s,-1);
+	std::cout<<Ans;
 	return 0;
 }
