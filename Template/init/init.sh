9412ac4d6be4629e80e3cefd1b0817bfdcfa734c
powerLEO101 <3256870400@qq.com>
2018-02-04 08:07:50 +0800

    new file:   Template/init/init.sh

---
+++ Template/init/init.sh
@@ -0,0 +1,20 @@
+sudo apt-get install unity-tweak-tool 
+sudo add-apt-repository ppa:noobslab/themes
+sudo add-apt-repository ppa:numix/ppa
+sudo apt-get update
+sudo apt-get install flatabulous-theme
+sudo apt-get install numix-gtk-theme numix-icon-theme-circle
+sudo apt-get install zsh git
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh 
+chsh -s /bin/zsh  
+echo "export ZSH=\$HOME/.oh-my-zsh\n\
+ZSH_THEME=\"robbyrussell\"\n\
+plugins=(git)\n\
+source \$ZSH/oh-my-zsh.sh\n\
+export PATH=\$HOME/bin:/usr/local/bin:\$PATH" >> ~/.zshrc
+chsh -s /bin/zsh  
+ssh-keygen -t rsa -C "3256870400@qq.com"
+sudo add-apt-repository ppa:a-v-shkop/chromium
+sudo apt-get update
+sudo apt-get install chromium-browser
+
