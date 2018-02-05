1c72935aaa2e439d01bcb873bce1452ff2cbf08a
powerLEO101 <3256870400@qq.com>
2018-02-05 10:19:56 +0800

    modified:   CEDL_test/C1-huangzile18-2-4/time/time.cpp

--- CEDL_test/C1-huangzile18-2-4/time/time.cpp
+++ CEDL_test/C1-huangzile18-2-4/time/time.cpp
@@ -16,13 +16,43 @@ int get_int()
 }
 
 int P1[1001],P2[1001];
-int f1[10001],f2[10001];
-int s[1001];
+int f[101][101];
+
+void init()
+{
+	for(int i = 0;i<101;i++)
+	for(int j = 0;j<101;j++)
+		f[i][j] = -1;
+}
+
+bool check(int MID,int n,int m)
+{
+	init();
+	f[0][0] = 0;
+	for(int i = 1;i<=n;i++)
+	for(int j = 0;j<=m;j++)
+	for(int k = 0;k<=j;k++)
+		if(MID-P1[i]*k>=0&&f[i-1][j-k]!=-1)
+			f[i][j] = std::max(f[i][j],f[i-1][j-k]+(MID-P1[i]*k)/P2[i]);
+	if(f[n][m]>=m)return true;
+	return false;
+}
 
 int main()
 {
 	freopen("time.in","r",stdin);
 	freopen("time.out","w",stdout);
-	std::cout<<18;
+	int n = gi,m = gi;
+	for(int i = 1;i<=n;i++)
+		P1[i] = gi,
+		P2[i] = gi;
+	int l = 1,r = 10000;
+	while(l+1<r)
+	{
+		int MID = (l+r)/2;
+		if(check(MID,n,m)==true)r = MID;
+		else l = MID;
+	}
+	std::cout<<r;
 	return 0;
 }
