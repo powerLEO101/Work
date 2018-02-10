1314b3d397c6a89b5a23fa644181864f1baa31f5
powerLEO101 <3256870400@qq.com>
2018-02-10 11:10:06 +0800

    new file:   CEDL_test/C1-huangzile18-2-10/magic/magic.cpp

---
+++ CEDL_test/C1-huangzile18-2-10/math/math.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<iomanip>
+#include<fstream>
+#include<cstdio>
+#include<algorithm>
+#define gi get_int()
+
+int get_int()
+{
+	int x = 0,y = 1;char ch = getchar();
+	while((ch<'0'||ch>'9')&&ch!='-')ch = getchar();
+	if(ch=='-')y = -1,ch = getchar();
+	while(ch>='0'&&ch<='9')x = x*10+ch-'0',ch = getchar();
+	return x*y;
+}
+
+int main()
+{
+	freopen("math.in","r",stdin);
+	freopen("math.out","w",stdout);
+	
+	return 0;
+}
