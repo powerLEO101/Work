42c6f9d993a1653640100e5cec98486bbfbfeb05
powerLEO101 <3256870400@qq.com>
2018-04-05 20:24:30 +0800

    Fixed 2018-4-5Test Bus Problem Update Ubuntu_init File

--- CEDL_test/C1-huangzile2018-4-5/bus.cpp
+++ CEDL_test/C1-huangzile2018-4-5/bus.cpp
@@ -18,42 +18,45 @@ class Edge
 {
 public:
 	int Next,To,Val,Index;
-}Edges[4001];
-int Head[4001],Val[4001],E_num;
+}Edges[100000 << 1];
+int Head[100000 << 1],E_num;
 void Add_edge(int From,int To,int val,int Index)
 {
 	Edges[E_num] = (Edge){Head[From],To,val,Index};
 	Head[From] = E_num++;
 }
 
-int T[1001][1001],Size[1001],Vis[1001];
+int T[100000 << 1],Dist[100000 << 1];
+bool Vis[100000 << 1];
 
-int Get_ans(int x)
+int Get_ans()
 {
+	memset(Dist,0x3f,sizeof(Dist));
 	memset(Vis,0,sizeof(Vis));
-	std::queue<int> Q,Time;
+	std::queue<int> Q;
 	Q.push(0);
-	Time.push(0);
+	Vis[0] = true;
+	Dist[0] = 0;
 	while(!Q.empty())
 	{
 		int Now = Q.front();Q.pop();
-		int Tim = Time.front();Time.pop();
 		for_edge(i,Now)
 		{
-			int v = Edges[i].To,Val = Edges[i].Val,Index = Edges[i].Index;
-			if(Tim%Size[Index]!=Val)continue;
-			if(v==x)return Tim+1;
-			if(Vis[v]>300)continue;
-			Vis[v]++;
-			Q.push(v);
-			Time.push(Tim+1);
+			int v = Edges[i].To,Val = Edges[i].Val,Index = Edges[i].Index,w;
+			if(Dist[Now]%Val<=Index)w = Index-(Dist[Now]%Val)+1;
+			else w = Val-(Dist[Now]%Val-Index)+1;
+			if(Dist[v]>Dist[Now]+w)
+			{
+				Dist[v] = Dist[Now]+w;
+				if(Vis[v]==false)
+				{
+					Vis[v] = true;
+					Q.push(v);
+				}
+			}
 		}
-		if(Vis[Now]>300)continue;
-		Vis[Now]++;
-		Q.push(Now);
-		Time.push(Tim+1);
+		Vis[Now] = false;
 	}
-	return -1;
 }
 
 int main()
@@ -64,13 +67,12 @@ int main()
 	int n = gi,m = gi;
 	for(int i = 0;i<m;i++)
 	{
-		Size[i] = gi;
-		for(int j = 0;j<Size[i];j++)T[i][j] = gi-1;
-		for(int j = 0;j<Size[i]-1;j++)
-			Add_edge(T[i][j],T[i][j+1],j,i);
-		Add_edge(T[i][Size[i]-1],T[i][0],Size[i]-1,i);
+		int K = gi;
+		for(int j = 0;j<K;j++)T[j] = gi-1;
+		T[K] = T[0];
+		for(int j = 0;j<K;j++)Add_edge(T[j],T[j+1],K,j);
 	}
-	for(int i = 1;i<n;i++)
-		std::cout<<Get_ans(i)<<" ";
+	Get_ans();
+	for(int i = 1;i<n;i++)std::cout<<(Dist[i]>1e9?-1:Dist[i])<<" ";
 	return 0;
 }
