6b097ae0a3e851b30808f393b7c4e9b730b884a3
powerLEO101 <powerLEO101@outlook.com>
2018-08-20 15:35:53 +0800

    Update code LOJ10113

--- Code/LOJ/LOJ10113/code.cpp
+++ Code/LOJ/LOJ10113/code.cpp
@@ -4,9 +4,10 @@
 #include <cstring>
 #define File(s) freopen(#s".in", "r", stdin); freopen(#s".out", "w", stdout)
 #define gi get_int()
-int get_int()
+#define _ 2000005
+long long get_int()
 {
-	int x = 0, y = 1; char ch = getchar();
+	long long x = 0, y = 1; char ch = getchar();
 	while ((ch < '0' || ch > '9') && ch != '-')
 		ch = getchar();
 	if (ch == '-') y = -1,ch = getchar();
@@ -17,68 +18,41 @@ int get_int()
 	return x * y;
 }
 
-class Node
+long long C[_], n, m;
+int Lowbit(int u) {return u & (-u);}
+void Add(int Index,long long Value)
 {
-public:
-	Node* l_son,* r_son;
-	int Value, l, r;
-Node() {
-	l_son = r_son = NULL;
-	Value = l = r = 0;
-}
-}* Tree = new Node;
-void Build(Node* Root = Tree)
-{
-	if (Root -> r == Root -> l + 1) {
-		Root -> Value = gi;
-		return;
+	while (Index <= n) {
+		C[Index] += Value;
+		Index += Lowbit(Index);
 	}
-	Root -> l_son = new Node;
-	Root -> r_son = new Node;
-	Root -> l_son -> l = Root -> l;
-	Root -> l_son -> r = (Root -> l + Root -> r) / 2;
-	Root -> r_son -> l = (Root -> l + Root -> r) / 2;
-	Root -> r_son -> r = Root -> r;
-	Build(Root -> l_son);
-	Build(Root -> r_son);
-	Root -> Value = Root -> l_son -> Value +\
-			Root -> r_son -> Value;
 }
-void Change(int Index, int Val, Node* Root = Tree)
+long long Sum(int Index)
 {
-	if (Index < Root -> l || Index >= Root -> r) return;
-	if (Root -> r == Root -> l + 1) {
-		Root -> Value += Val;
-		return;
+	long long Ans = 0;
+	while(Index > 0) {
+		Ans += C[Index];
+		Index -= Lowbit(Index);
 	}
-	Change(Index, Val, Root -> l_son);
-	Change(Index, Val, Root -> r_son);
-	Root -> Value = Root -> l_son -> Value +\
-			Root -> r_son -> Value;
-}
-int Query(int l, int r, Node* Root = Tree)
-{
-	if (r <= Root -> l || Root -> r <= l) return 0;
-	if (l <= Root -> l && Root -> r <= r) return Root -> Value;
-	return Query(l, r, Root -> l_son) +\
-		Query(l, r, Root -> r_son);
+	return Ans;
 }
 
 int main()
 {
 	File(code);
-	int n = gi, m = gi;
-	Tree -> l = 0;
-	Tree -> r = n;
-	Build();
+	n = gi, m = gi;
+	for (int i = 0; i < n; i++) {
+		long long Value = gi;
+		Add(i + 1, Value);
+	}
 	for (int i = 0; i < m; i++) {
 		int Mod = gi;
-		if (Mod == 0) {
-			int l = gi - 1,r = gi;
-			printf("%d\n", Query(l, r));
+		if (Mod == 1) {
+			long long Index = gi, Val = gi;
+			Add(Index, Val);
 		} else {
-			int Index = gi - 1,Val = gi;
-			Change(Index, Val);
+			long long l = gi, r = gi;
+			printf("%lld\n", Sum(r) - Sum(l - 1));
 		}
 	}
 	return 0;
