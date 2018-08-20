61ca53cc815df72f5b0a1f0305915d584e5efed5
powerLEO101 <powerLEO101@outlook.com>
2018-08-20 15:53:02 +0800

    AC LOJ10115

--- Code/LOJ/LOJ10115/code.cpp
+++ Code/LOJ/LOJ10115/code.cpp
@@ -17,65 +17,38 @@ int get_int()
 	return x * y;
 }
 
-class Node
+int Left[500000], Right[500000], n, m;
+int Lowbit(int u) {return u & (-u);}
+void Add(int Index, int Value, int C[])
 {
-public:
-	Node* l_son,* r_son;
-	int l, r, Value;
-Node() {
-	l_son = r_son = NULL;
-	l = r = Value = 0;
-}
-}* Left = new Node,* Right = new Node;
-void Build(Node* Root)
-{
-	if (Root -> r == Root -> l + 1) return;
-	Root -> l_son = new Node;
-	Root -> r_son = new Node;
-	Root -> l_son -> l = Root -> l;
-	Root -> l_son -> r = (Root -> l + Root -> r) / 2;
-	Root -> r_son -> l = (Root -> l + Root -> r) / 2;
-	Root -> r_son -> r = Root -> r;
-	Build(Root -> l_son);
-	Build(Root -> r_son);
-}
-void Change(int Index, int Val, Node* Root)
-{
-	if (Root -> r <= Index || Root -> l > Index) return;
-	if (Root -> r == Root -> l + 1) {
-		Root -> Value += Val;
-		return;
+	while (Index <= n) {
+		C[Index] += Value;
+		Index += Lowbit(Index);
 	}
-	Change(Index, Val, Root -> l_son);
-	Change(Index, Val, Root -> r_son);
-	Root -> Value = Root -> l_son -> Value +\
-			Root -> r_son -> Value;
 }
-int Query(int l, int r, Node* Root)
+int Sum(int Index, int C[])
 {
-	if (Root -> r <= l || r <= Root -> l) return 0;
-	if (l <= Root -> l && Root -> r <= r) return Root -> Value;
-	return Query(l, r, Root -> l_son) +\
-		Query(l, r, Root -> r_son);
+	int Ans = 0;
+	while (Index > 0) {
+		Ans += C[Index];
+		Index -= Lowbit(Index);
+	}
+	return Ans;
 }
 
 int main()
 {
 	File(code);
-	Left -> l = Right -> l = 0;
-	Left -> r = Right -> r = 50001;
-	Build(Left);
-	Build(Right);
-	int n = gi, m = gi;
+	n = gi, m = gi;
 	for (int i = 0; i < m; i++) {
 		int Mod = gi;
 		if (Mod == 1) {
 			int l = gi, r = gi;
-			Change(l, 1, Left);
-			Change(r, 1, Right);
+			Add(l, 1, Left);
+			Add(r, 1, Right);
 		} else {
 			int l = gi, r = gi;
-			printf("%d\n", Query(0, r + 1, Left) - Query(0, l, Right));
+			printf("%d\n", Sum(r, Left) - Sum(l - 1, Right));
 		}
 	}
 	return 0;
