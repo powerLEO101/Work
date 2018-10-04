ac743edfa2c6f9114a5e63afb6f7a1f63df6b362
powerLEO101 <powerLEO101@outlook.com>
2018-10-04 15:02:05 +0800

    AC LOJ6278

--- Code/LOJ/LOJ6278/code.cpp
+++ Code/LOJ/LOJ6278/code.cpp
@@ -11,28 +11,29 @@
 #include <vector>
 #define File(s) freopen(#s".in", "r", stdin); freopen(#s".out", "w", stdout)
 #define gi get_int()
-int get_int()
+#define _ 200000
+long long get_int()
 {
-	int x = 0, y = 1; char ch = getchar();
+	long long x = 0, y = 1; char ch = getchar();
 	while ((ch < '0' || ch > '9') && ch != '-') ch = getchar();
 	if (ch == '-') y = -1, ch = getchar();
 	while (ch <= '9' && ch >= '0') x = x * 10 + ch - '0', ch = getchar();
 	return x * y;
 }
 
-int a[_], Len, Tag[_];
-std::vector<int> Num[_];
+long long a[_], Len, Tag[_], Belong[_], n;
+std::vector<long long> Num[_];
 
 void Update(int x)
 {
 	Num[x].clear();
-	for (int i = Belong[x] * Len; i < Belong[x + 1] * Len; i++)
-		Num[x].push_back(Num[i]);
+	for (int i = x * Len; i < (x + 1) * Len && i < n; i++)
+		Num[x].push_back(a[i]);
 	std::sort(Num[x].begin(), Num[x].end());
 }
-void Modify(int l, int r, int Value)
+void Modify(int l, int r, long long Value)
 {
-	for (int i = Belong[l] * Len; i < (Belong[l] + 1) * Len && i <= r; i++) a[i] += Value;
+	for (int i = l; i < (Belong[l] + 1) * Len && i <= r; i++) a[i] += Value;
 	Update(Belong[l]);
 
 	if (Belong[l] != Belong[r]) {
@@ -40,22 +41,22 @@ void Modify(int l, int r, int Value)
 		Update(Belong[r]);
 	}
 
-	for (int i = Belong[l] + 1; i < Belong[r]; i++) Tag[Belong[i]] += Value;
+	for (int i = Belong[l] + 1; i < Belong[r]; i++) Tag[i] += Value;
 }
-int Query(int l, int r, int Value)
+int Query(int l, int r, long long Value)
 {
 	int Ans = 0;
 
-	for (int i = Belong[l] * Len; i < (Belong[l] + 1) * Len && i <= r; i++) 
-		Ans += (a[i] + Tag[Belong[i]]) <= Value;
+	for (int i = l; i < (Belong[l] + 1) * Len && i <= r; i++) 
+		Ans += (a[i] + Tag[Belong[l]]) < Value;
 
 	if (Belong[l] != Belong[r]) {
 		for (int i = Belong[r] * Len; i <= r; i++) 
-			Ans += (a[i] + Tag[Belong[i]]) <= Value;
+			Ans += (a[i] + Tag[Belong[r]]) < Value;
 	}
 
 	for (int i = Belong[l] + 1; i < Belong[r]; i++)
-		Ans += std::lower_bound(Num[i].begin(), Num[i].end(), Value - Tag[i]) - Num[i].begin();
+		Ans += std::lower_bound(Num[i].begin(), Num[i].end(), 1ll * Value - Tag[i]) - Num[i].begin();
 	
 	return Ans;
 }
@@ -64,16 +65,19 @@ int main()
 {
 	File(code);
 
-	int n = gi;
+	n = gi;
 	Len = sqrt(n) / 2;
+
 	for (int i = 0; i < n; i++) a[i] = gi, Belong[i] = i / Len, Num[Belong[i]].push_back(a[i]);
 
+	for (int i = 0; i <= Belong[n - 1]; i++) std::sort(Num[i].begin(), Num[i].end());
+
 	for (int i = 0; i < n; i++) {
-		int opt = gi, l = gi, r = gi, c = gi;
+		int opt = gi, l = gi - 1, r = gi - 1, c = gi;
 		if (opt == 0) {
 			Modify(l, r, c);
 		} else {
-			printf("%d\n", Query(l, r, c));
+			printf("%d\n", Query(l, r, 1ll * c * c));
 		}
 	}
 
