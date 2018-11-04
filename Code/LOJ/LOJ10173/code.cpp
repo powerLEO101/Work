d074e8af26e1be9f7ec677b398ddf1253000f6f4
powerLEO101 <powerLEO101@outlook.com>
2018-11-05 07:59:40 +0800

    Coding...

--- Code/LOJ/LOJ10173/code.cpp
+++ Code/LOJ/LOJ10173/code.cpp
@@ -23,7 +23,17 @@ int get_int()
 	return x * y;
 }
 
-int Num[MaxVal], Dp[MaxN][MaxVal];
+int Num[MaxVal], Dp[MaxN][MaxVal][MaxVal], Sum[MaxVal], Len;
+
+int Get_Sum(int Val)
+{
+	int Ret = 0;
+	while (Val != 0) {
+		Ret += Val % 2;
+		Val >>= 1;
+	}
+	return Sum[Len] = Ret;
+}
 
 int main()
 {
@@ -43,29 +53,33 @@ int main()
 
 	for (int i = 0; i < (1 << m); i++) {
 		if ((i & (i << 1)) || (i & (i << 2))) continue;
-		Dp[0][Len] = 1;
+		if ((Status[0] & i) == 0) Dp[0][Len][0] = Get_Sum(i);
 		Num[Len++] = i;
 	}
 
 	for (int i = 0; i < Len; i++) {
 		int x = Num[i];
+		if (x & Status[1]) continue;
 		for (int j = 0; j < Len; j++) {
 			int y = Num[j];
+			if (f & Status[0]) continue;
 			if (x & y) continue;
-			Dp[1][i] += Dp[0][j];
+			Dp[1][i][j] = Sum[i] + Sum[j];
 		}
 	}
 	for (int i = 2; i < n; i++)
 		for (int j = 0; j < Len; j++) {
 			int x = Num[j];
-			if ((x & Status[i]) != 0) continue;
+			if (x & Status[i]) continue;
 			for (int k = 0; k < Len; k++) {
 				int y = Num[k];
+				if (y & Statys[i - 1]) continue;
 				if ((y & Status[i - 1]) != 0) continue;
 				for (int l = 0; l < Len; l++) {
 					int z = Num[l];
 					if ((z & Status[i - 2]) != 0) continue;
-					if (
+					if ((x & y) || (x & z) || (y & z)) continue;
+					Dp[i][j][k] = std::max(Dp[i][j][k], Dp[i - 1][j - 1
 				}
 			}
 		}
