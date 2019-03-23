c7c10f6b19453d7ae5fb5d579283fcd9a56e3f41
powerLEO101 <powerLEO101@outlook.com>
2019-03-23 15:18:48 +0800

    AC LG3757

--- Code/LG/LG3757/code.cpp
+++ Code/LG/LG3757/code.cpp
@@ -9,6 +9,8 @@
 #include <cstring>
 #define gi get_int()
 #define _FILE(s) freopen(#s".in", "r", stdin); freopen(#s".out", "w", stdout)
+#define MOD 1000000007
+const int MAXN = 205;
 int get_int()
 {
 	int x = 0, y = 1; char ch = getchar();
@@ -19,40 +21,48 @@ int get_int()
 }
 
 int n;
+char type[MAXN];
+int size[MAXN], dp[MAXN][MAXN], sum[MAXN][MAXN][2], num[MAXN][MAXN];
 void dfs(int now = 1)
 {
 	int son1 = now << 1, son2 = (now << 1) + 1;
 	if (son1 > n && son2 > n) {
 		size[now] = 1;
-		dp[i][1] = 1;
+		dp[now][1] = sum[now][1][0] = sum[now][1][1] = 1;
 		return ;
 	}
 	if (son2 > n) {
+		dfs(son1);
 		size[now] = size[son1] + 1;
 		for (int i = 1; i <= size[now]; i++) {
 			if (type[son1] == '<') 
-				dp[now][i] = (dp[now][i] + sum[son1][i][1]) % MOD;
+				dp[now][i] += (dp[now][i] + sum[son1][i][1]) % MOD;
 			else 
-				dp[now][i] = (dp[now][i] + sum[son1][j - 1][0]) % MOD;
+				dp[now][i] += (dp[now][i] + sum[son1][i - 1][0]) % MOD;
 		}
 		for (int i = 1; i <= size[now]; i++)
 			sum[now][i][0] = (sum[now][i - 1][0] + dp[now][i]) % MOD;
 		for (int i = size[now]; i > 0; i--)
 			sum[now][i][1] = (sum[now][i + 1][1] + dp[now][i]) % MOD;
-		return 0;
+		return ;
 	}
+	dfs(son1); dfs(son2);
 	size[now] = size[son1] + size[son2] + 1;
 	for (int i = 1; i <= size[now]; i++) {
 		for (int j = 0; j < i && j <= size[son1]; j++) {
-			int k = j - 1 - j, s1, s2;
+			int k = i - 1 - j, s1, s2;
 			if (k > size[son2]) continue;
 			if (type[son1] == '<') s1 = sum[son1][j + 1][1];
 			else s1 = sum[son1][j][0];
 			if (type[son2] == '<') s2 = sum[son2][k + 1][1];
 			else s2 = sum[son2][k][0];
-			dp[now][i] = (1ll * dp[now][i] + (1ll * s1 * s2) 
+			dp[now][i] = (1ll * dp[now][i] + (((((1ll * s1 * s2) % MOD) * num[i - 1][j]) % MOD) * num[size[now] - i][size[son1] - j]) % MOD) % MOD;
 		}
 	}
+	for (int i = 1; i <= size[now]; i++)
+		sum[now][i][0] = (sum[now][i - 1][0] + dp[now][i]) % MOD;
+	for (int i = size[now]; i > 0; i--)
+		sum[now][i][1] = (sum[now][i + 1][1] + dp[now][i]) % MOD;
 }
 
 int main()
@@ -61,6 +71,17 @@ int main()
 
 	n = gi;
 	scanf("%s", type + 2);
+
+	num[0][0] = 1;
+	for (int i = 1; i <= n; i++) {
+		num[i][0] = 1;
+		for (int j = 1; j < i; j++)
+			num[i][j] = (num[i - 1][j] + num[i - 1][j - 1]) % MOD;
+		num[i][i] = 1;
+	}
+	dfs();
+
+	printf("%d", sum[1][size[1]][0]);
 
 	return 0;
 }
