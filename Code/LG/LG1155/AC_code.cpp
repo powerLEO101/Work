f6b91e0bce343e89c85e35c77e066e853ac3eba4
powerLEO101 <powerleo101@outlook.com>
2019-04-15 23:16:58 +0800

    AC LG1155

--- Code/LG/LG1155/AC_code.cpp
+++ Code/LG/LG1155/AC_code.cpp
@@ -1,59 +1,114 @@
 #include <iostream>
 #include <cstdio>
+#include <cstdlib>
 #include <algorithm>
 #include <cstring>
-using namespace std;
-const int N=1005;
-inline int read(){
-	char c=getchar();int x=0,f=1;
-	while(c<'0'||c>'9'){if(c=='-')f=-1;c=getchar();}
-	while(c>='0'&&c<='9'){x=x*10+c-'0';c=getchar();}
-	return x*f;
-}
-int n,a[N];
-struct edge{
-	int v,ne;
-}e[N*N];
-int h[N],cnt=0;
-inline void ins(int u,int v){
-	cnt++;
-	e[cnt].v=v;e[cnt].ne=h[u];h[u]=cnt;
-	cnt++;
-	e[cnt].v=u;e[cnt].ne=h[v];h[v]=cnt;
-}
-int f[N];
-void dp(){
-	f[n]=a[n];
-	for(int i=n-1;i>=1;i--) f[i]=min(f[i+1],a[i]);
-	for(int i=1;i<=n;i++)
-		for(int j=i+1;j<=n;j++)
-			if(a[i]<a[j]&&f[j]<a[i]) ins(i,j);
-}
-int col[N];
-bool color(int u,int c){
-	col[u]=c;
-	for(int i=h[u];i;i=e[i].ne){
-		int v=e[i].v;
-		if(col[v]==col[u]) return false;
-		if(!col[v]&&!color(v,3-c)) return false;
-	}
-	return true;
-}
-int s1[N],t1=0,s2[N],t2=0;
-int main(){
-	n=read();
-	for(int i=1;i<=n;i++) a[i]=read();
-	dp();
-	for(int i=1;i<=n;i++) if(!col[i]&&!color(i,1)) {putchar('0');return 0;}
+#include <queue>
+#include <stack>
+#define zjp_shadow
 
-	int cur=1;
-	for(int i=1;i<=n;i++){
-		if(col[i]==1) s1[++t1]=a[i],printf("a ");
-		else s2[++t2]=a[i],printf("c ");
-		while(s1[t1]==cur||s2[t2]==cur){
-			if(s1[t1]==cur){printf("b ");t1--;}
-			else {printf("d ");t2--;}
-			cur++; 
+#define For(i, l, r) for( int i = (l), i##end = (int)(r); i <= i##end; ++i)
+#define Fordown(i, r, l) for( int i = (r), i##end = (int)(l); i >= i##end; --i)
+#define Set(a, v) memset(a, v, sizeof(a))
+#define Cpy(a, b) memcpy(a, b, sizeof(a))
+#define debug(x) cout << #x << ": " << (x) << endl
+#define DEBUG(...) fprintf(stderr, __VA_ARGS__)
+#define pb push_back
+
+using namespace std;
+
+template<typename T> inline bool chkmin(T &a, T b) { return b < a ? a = b, 1 : 0; }
+template<typename T> inline bool chkmax(T &a, T b) { return b > a ? a = b, 1 : 0; }
+
+inline int read() {
+	int x(0), sgn(1); char ch(getchar());
+	for (; !isdigit(ch); ch = getchar()) if (ch == '-') sgn = -1;
+	for (; isdigit(ch); ch = getchar()) x = (x * 10) + (ch ^ 48);
+	return x * sgn;
+}
+
+void File() {
+#ifdef zjp_shadow
+	freopen ("code.in", "r", stdin);
+	freopen ("AC_code.out", "w", stdout);
+#endif
+}
+
+const int N = 1010, inf = 0x7f7f7f7f;
+
+int n, P[N], minv[N], col[N];
+
+int pos = 1;
+stack<int> S[2];
+
+inline void out(char ch) {
+	putchar (ch); putchar (' ');
+}
+
+inline bool Pop(int id) {
+	if (!S[id].empty() && S[id].top() == pos) {
+		out(id ? 'd' : 'b'), S[id].pop(), ++ pos;
+		return true;
+	}
+	return false;
+}
+
+inline void Push(int cur, int id) {
+	if (id == 1) { while(Pop(0)); }
+	while (!S[id].empty() && S[id].top() < cur)
+		if (!Pop(id)) Pop(id ^ 1);
+	if (id == 1) { while(Pop(0)); }
+	S[id].push(cur); out(id ? 'c' : 'a');
+}
+
+vector<int> G[N];
+
+int main () {
+
+	File(); 
+	n = read();
+
+	For (i, 1, n)
+		P[i] = read();
+
+	minv[n + 1] = n + 1;
+	Fordown (i, n, 1)
+		minv[i] = min(minv[i + 1], P[i]);
+
+	For (i, 1, n) For (j, i + 1, n) 
+		if (minv[j + 1] < P[i] && P[i] < P[j]) {
+			G[i].pb(j), G[j].pb(i), col[i] = col[j] = -1;
+		}
+
+	for (int i = 1; i <= n; i++) printf("%d ", col[i]);
+	printf("\n");
+
+	For (i, 1, n) if (!~col[i]) {
+		queue<int> Q; Q.push(i); col[i] = 0;
+		while (!Q.empty()) {
+			int u = Q.front(); Q.pop();
+			for (int v : G[u]) {
+				if (~col[v] && col[v] != (col[u] ^ 1)) return puts("0"), 0;
+				if (!~col[v]) Q.push(v);
+				col[v] = col[u] ^ 1;
+			}
 		}
 	}
+
+	memset(col, 0, sizeof(col));
+	for (int i = 1; i <= n; i++)
+		printf("%d ", col[i]);
+
+	For (i, 1, n)
+		Push(P[i], col[i]);
+
+	bool flag = true;
+	while (flag) {
+		flag = false;
+		while(Pop(0)) flag = true;
+		while(Pop(1)) flag = true;
+	}
+
+	return 0;
+
 }
--- Code/LG/LG1155/code.cpp
+++ Code/LG/LG1155/code.cpp
@@ -7,8 +7,11 @@
 #include <cstdio>
 #include <algorithm>
 #include <cstring>
+#include <stack>
+#include <queue>
 #define _FILE(s) freopen(#s".in", "r", stdin); freopen(#s".out", "w", stdout)
 #define gi get_int()
+const int MAXN = 1001, MAXM = 1001 * 1001;
 int get_int()
 {
 	int x = 0, y = 1; char ch = getchar();
@@ -18,18 +21,95 @@ int get_int()
 	return x * y;
 }
 
-int minValue[MAXN];
+int minValue[MAXN], color[MAXN], num[MAXN], pos = 1;
+std :: stack<int> stack[2];
+
+class Edge
+{
+public:
+	int next, to;
+} edges[MAXM];
+int head[MAXN], edgesNum;
+void addEdge(int from, int to)
+{
+	edges[edgesNum] = (Edge) {head[from], to};
+	head[from] = edgesNum++;
+}
+
+bool vis[MAXN];
+void match(int now)
+{
+	std :: queue<int> q;
+	q.push(now);
+	while (!q.empty()) {
+		int u = q.front();
+		q.pop();
+		for (int i = head[u]; i != -1; i = edges[i].next) {
+			int to = edges[i].to;
+			if (~color[to] && color[to] != (color[u] ^ 1)) {
+				printf("0");
+				exit(0);
+			}
+			if (!~color[to]) q.push(to);
+			color[to] = color[u] ^ 1;
+		}
+	}
+}
+
+bool pop(int col)
+{
+	if (!stack[col].empty() && stack[col].top() == pos) {
+		printf("%c ", col == 1 ? 'd' : 'b');
+		stack[col].pop();
+		pos++;
+		return true;
+	}
+	return false;
+}
+bool popUntil(int col)
+{
+	bool flag = false;
+	while (pop(col)) flag = true;
+	return flag;
+}
+void pushIntoStack(int now, int col)
+{
+	if (col == 1) popUntil(0);
+	while (!stack[col].empty() && stack[col].top() < now)
+		if (!pop(col)) pop(col ^ 1);
+	if (col == 1) popUntil(0);
+	stack[col].push(now); 
+	printf("%c ", col == 1 ? 'c' : 'a');
+}
 
 int main()
 {
 	_FILE(code);
 
+	memset(head, -1, sizeof(head));
+
 	int n = gi;
 	for (int i = 1; i <= n; i++) num[i] = gi;
-	minValue[n] = num[n];
-	for (int i = n - 1; i >= 1; i--) 
+	minValue[n + 1] = n + 1;
+	for (int i = n; i >= 1; i--)
 		minValue[i] = std :: min(minValue[i + 1], num[i]);
 	for (int i = 1; i <= n; i++)
+		for (int j = i + 1; j <= n; j++)
+			if (minValue[j + 1] < num[i] && num[i] < num[j]) {
+				addEdge(i, j); addEdge(j, i);
+				color[i] = color[j] = -1;
+			}
+
+	for (int i = 1; i <= n; i++) {
+		if (color[i] != -1) continue;
+		color[i] = 0;
+		match(i);
+	}
+
+	for (int i = 1; i <= n; i++)
+		pushIntoStack(num[i], color[i]);
+
+	while (popUntil(0) || popUntil(1)) ;
 
 	return 0;
 }
