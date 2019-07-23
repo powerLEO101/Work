cde99fd5f7b8fb8249fa5748de7e10aa112e0d60
cjoier <poewrLEO101@outlook.com>
2019-07-23 19:47:40 +0800

    AC LG3245

--- Code/LG/LG3245/code.cpp
+++ Code/LG/LG3245/code.cpp
@@ -25,7 +25,8 @@ int get_int()
 	return x * y;
 }
 
-int num[LIMIT], answer[LIMIT], sum[LIMIT], s[LIMIT], count[10];
+int num[LIMIT], answer[LIMIT], sum[LIMIT], s[LIMIT], count[LIMIT];
+int jud[LIMIT], f[LIMIT], g[LIMIT]; 
 int ans, block;
 
 class data
@@ -63,11 +64,27 @@ signed main()
 	std :: string str;
 	std :: cin >> str;
 	int n = str.size();
+	if (p == 2 || p == 5) {
+		if (p == 2) 
+			jud[0] = jud[2] = jud[4] = jud[6] = jud[8] = 1;
+		if (p == 5)
+			jud[0] = jud[5] = 1;
+		for (int i = 1; i <= n; i++) {
+			f[i] = f[i - 1] + jud[str[i - 1] - '0'];
+			g[i] = g[i - 1] + jud[str[i - 1] - '0'] * (i);
+		}
+		int m = gi;
+		while (m--) {
+			int l = gi, r = gi;
+			printf("%lld\n", g[r] - g[l - 1] - (f[r] - f[l - 1]) * (l - 1));
+		}
+		return 0;
+	}
 	block = sqrt(n);
-	for (int i = 0; i < n; i++) 
-		num[i] = str[i] - '0';
+	for (int i = 0; i < n; i++)
+		num[i] = str[i] ^ '0';
 	sum[n - 1] = num[n - 1] % p;
-	for (int i = n - 2, c = 10; i >= 0; i--, c = (c * 10) % p)
+	for (int i = n - 1, c = 1; i >= 0; i--, c = (c * 10) % p)
 		sum[i] = (sum[i + 1] + num[i] * c) % p,
 		s[i + 1] = sum[i];
 	std :: sort(s + 1, s + n + 2);
@@ -76,11 +93,6 @@ signed main()
 		sum[i - 1] = std :: lower_bound(s + 1, s + t + 1, sum[i - 1]) - s;
 	for (int i = n; i >= 0; i--)
 		sum[i + 1] = sum[i];
-
-	printf("%d\n", t);
-	for (int i = 1; i <= n + 1; i++)
-		printf("%d ", sum[i]);
-	printf("\n");
 
 	int m = gi;
 	for (int i = 0; i < m; i++) {
@@ -97,14 +109,10 @@ signed main()
 		while (r < q[i].r) add(++r);
 		while (r > q[i].r) del(r--);
 		answer[q[i].id] = ans;
-		printf("%d %d\n", q[i].l, q[i].r);
-		printf("%d\n", ans);
 	}
 
-	/*
 	for (int i = 0; i < m; i++)
 		printf("%lld\n", answer[i]);
-		*/
 
 	return 0;
 }
