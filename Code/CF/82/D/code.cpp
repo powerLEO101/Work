67973d5ec0404fbc7e5205880e2b699f619dd7d9
powerLEO101 <powerleo101@outlook.com>
2020-11-25 14:41:18 +0800

    AC CF82D

--- Code/CF/82/D/code.cpp
+++ Code/CF/82/D/code.cpp
@@ -13,11 +13,14 @@ int get_int()
 	return x * y;
 }
 
-int f[MAXN][MAXN], num[MAXN], path[MAXN][MAXN][3];
+int n, f[MAXN][MAXN], num[MAXN], path[MAXN][MAXN][3];
 
 void outPath(int x, int y)
 {
-	
+	if (x != 1) outPath(x - 1, path[x][y][2]);
+	if (path[x][y][0] > n) std::cout << path[x][y][1] << std::endl;
+	else if (path[x][y][1] > n) std::cout << path[x][y][0] << std::endl;
+	else std::cout << path[x][y][0] << ' ' << path[x][y][1] << std::endl;
 }
 
 int main()
@@ -25,7 +28,7 @@ int main()
 	freopen("code.in", "r", stdin);
 	freopen("code.out", "w", stdout);
 
-	int n = gi;
+	n = gi;
 	for (int i = 1; i <= n; i++)
 		num[i] = gi;
 	n++;
@@ -36,20 +39,26 @@ int main()
 		for (int j = 1; j < a; j++) {
 			if (f[i - 1][j] + std::max(num[a], num[b]) < f[i][j]) {
 				f[i][j] = f[i - 1][j] + std::max(num[a], num[b]);
-				path[i][j] = {a, b, j};
+				path[i][j][0] = a;
+				path[i][j][1] = b;
+				path[i][j][2] = j;
 			}
 			if (f[i - 1][j] + std::max(num[a], num[j]) < f[i][b]) {
 				f[i][b] = f[i - 1][j] + std::max(num[a], num[j]);
-				path[i][b] = {a, j, j};
+				path[i][b][0] = a;
+				path[i][b][1] = j;
+				path[i][b][2] = j;
 			}
 			if (f[i - 1][j] + std::max(num[b], num[j]) < f[i][a]) {
 				f[i][a] = f[i - 1][j] + std::max(num[b], num[j]);
-				path[i][a] = {b, j, j};
+				path[i][a][0] = b;
+				path[i][a][1] = j;
+				path[i][a][2] = j;
 			}
 		}
 	}
 	
-	std::cout << f[n / 2][n];
+	std::cout << f[n / 2][n] << std::endl;
 	outPath(n / 2, n);
 
 	return 0;
